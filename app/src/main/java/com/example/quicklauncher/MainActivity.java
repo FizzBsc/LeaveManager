6242d1c559b6f9b65f3886b46c9c097bcc84b52b
Razaabbas897 <s3798278@student.rmit.edu.au>
2020-09-20 18:36:19 +1000

    Raza Abbas

--- app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -4,17 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Database myDb;
+    HolidayDB Hdb;
+    EditText editDate,editMonth,editYear,editDay;
+    Button btnAddData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new Database(this);
+        Hdb = new HolidayDB(this);
+
+        editDate = (EditText) findViewById(R.id.editText_DATE);
+        editMonth = (EditText) findViewById(R.id.editText_MONTH);
+        editYear = (EditText) findViewById(R.id.editText_YEAR);
+        editDay = (EditText) findViewById(R.id.editText_DAY);
+        btnAddData = (Button) findViewById(R.id.button_add);
+        AddDAta();
+
+
+
 
         Button leaveHistryBtn = (Button) findViewById(R.id.leaveHistoryBtn);
         leaveHistryBtn.setOnClickListener(new View.OnClickListener() {
@@ -97,5 +114,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public void AddDAta(){
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = Hdb.insertData(editDate.getText().toString(),
+                                editMonth.getText().toString(),
+                                editYear.getText().toString(),
+                                editDay.getText().toString());
+                        if(isInserted = true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+
+
+                    }
+                }
+
+
+        );
     }
 }
